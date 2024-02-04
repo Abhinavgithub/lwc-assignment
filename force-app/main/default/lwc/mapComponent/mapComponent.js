9262833cf807fe8cb551b2d8990a9e6368ee62e6
Abhinav Singh <adikari.abhinav@gmail.com>
2024-02-04 21:38:03 +0530

    getAddress Apex Method

--- force-app/main/default/lwc/mapComponent/mapComponent.js
+++ force-app/main/default/lwc/mapComponent/mapComponent.js
@@ -1,15 +1,20 @@
 import { LightningElement, wire } from "lwc";
 import {
   subscribe,
-  unsubscribe,
   APPLICATION_SCOPE,
   MessageContext
 } from "lightning/messageService";
 import msgService from "@salesforce/messageChannel/messageChannelName__c";
+import getAddress from "@salesforce/apex/QueryController.getAddress";
 
 export default class MapComponent extends LightningElement {
   recordId;
   recordType;
+  latitude;
+  longitude;
+  mapMarkers;
+  isMap = false;
+  error;
   @wire(MessageContext)
   messageContext;
 
@@ -31,5 +36,20 @@ export default class MapComponent extends LightningElement {
   handleMessage(message) {
     this.recordId = message.recordId;
     this.recordType = message.recordType;
+    this.getMapAddress(this.recordId, this.recordType);
+  }
+
+  async getMapAddress(recordId, recordType) {
+    try {
+      console.log(`recordId = ${recordId}  recordType= ${recordType}`);
+      const address = await getAddress({
+        recordId: recordId,
+        type: recordType
+      });
+      console.log(`address = ${address}`);
+    } catch (error) {
+      this.error = error;
+      console.log(`Error => ${error}`);
+    }
   }
 }
