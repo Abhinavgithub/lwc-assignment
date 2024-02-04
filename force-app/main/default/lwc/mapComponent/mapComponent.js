b4139a0ee2788afe9cb1dfaf7d5ea50476923fd8
Abhinav <adikari.abhinav@gmail.com>
2024-02-05 01:20:26 +0530

    MapComponent Created

--- force-app/main/default/lwc/mapComponent/mapComponent.js
+++ force-app/main/default/lwc/mapComponent/mapComponent.js
@@ -14,7 +14,9 @@ export default class MapComponent extends LightningElement {
   longitude;
   mapMarkers;
   isMap = false;
+  isLoading = false;
   error;
+  zoom = "18";
   @wire(MessageContext)
   messageContext;
 
@@ -41,12 +43,44 @@ export default class MapComponent extends LightningElement {
 
   async getMapAddress(recordId, recordType) {
     try {
+      this.isLoading = true;
+      let city, country, postalCode, state, street = null;
       console.log(`recordId = ${recordId}  recordType= ${recordType}`);
-      const address = await getAddress({
+      const address = JSON.parse(await getAddress({
         recordId: recordId,
         type: recordType
-      });
-      console.log(`address = ${address}`);
+      }));
+      console.log(`address = ${JSON.stringify(address)}`);
+      if (address.attributes.type == 'Contact') {
+        city = address.MailingAddress.city;
+        country = address.MailingAddress.country;
+        postalCode = address.MailingAddress.postalCode;
+        state = address.MailingAddress.state;
+        street = address.MailingAddress.street;
+      }
+      console.log(`${street} ${city} ${state} ${postalCode} ${country}`);
+
+      var requestOptions = {
+        method: 'GET',
+      };
+      fetch(`https://api.geoapify.com/v1/geocode/search?text=${street} ${city} ${state} ${postalCode} ${country}&apiKey=8c64e7d0ddb446438ff13248172b3dcb`, requestOptions)
+        .then(response => response.json())
+        .then(result => { console.log(result);          
+          this.latitude = result.features[0].properties.lat;
+          this.longitude = result.features[0].properties.lon;
+          this.mapMarkers = [
+            {
+                location: {
+                    Latitude: this.latitude,
+                    Longitude: this.longitude,
+                },
+            },            
+        ];
+        this.isLoading = false;        
+        this.isMap = true;  
+        console.log(`${this.latitude} ${this.longitude}`);
+         })
+        .catch(error => console.log('error', error));
     } catch (error) {
       this.error = error;
       console.log(`Error => ${error}`);
